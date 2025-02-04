c3b465f5b49fa68b10c025d38f3dd3df04019be7
JarodT77 <thongkhamdyjarod@gmail.com>
2025-02-04 01:19:27 +0100

    change image menu

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -7,23 +7,23 @@ function Menu() {
                 <h1 className="font-glegoo text-orange-600 text-4xl pb-4">Notre carte</h1>
             </div>
             <div className="flex flex-col justify-center items-center">
-                <img src="/public/images/Page-1.jpg"/>
-                <img src="/public/images/Page-2.jpg"/>
-                <img src="/public/images/Page-3.jpg"/>
-                <img src="/public/images/Page-4.jpg"/>
-                <img src="/public/images/Page-5.jpg"/>
-                <img src="/public/images/Page-6.jpg"/>
-                <img src="/public/images/Page-7.jpg"/>
-                <img src="/public/images/Page-8.jpg"/>
-                <img src="/public/images/Page-9.jpg"/>
-                <img src="/public/images/Page-10.jpg"/>
-                <img src="/public/images/Page-11.jpg"/>
-                <img src="/public/images/Page-12.jpg"/>
-                <img src="/public/images/Page-13.jpg"/>
-                <img src="/public/images/Page-14.jpg"/>
-                <img src="/public/images/Page-15.jpg"/>
-                <img src="/public/images/Page-16.jpg"/>
-                <img src="/public/images/vin.jpg"/>
+                <img src="/images/Page-1.jpg"/>
+                <img src="/images/Page-2.jpg"/>
+                <img src="/images/Page-3.jpg"/>
+                <img src="/images/Page-4.jpg"/>
+                <img src="/images/Page-5.jpg"/>
+                <img src="/images/Page-6.jpg"/>
+                <img src="/images/Page-7.jpg"/>
+                <img src="/images/Page-8.jpg"/>
+                <img src="/images/Page-9.jpg"/>
+                <img src="/images/Page-10.jpg"/>
+                <img src="/images/Page-11.jpg"/>
+                <img src="/images/Page-12.jpg"/>
+                <img src="/images/Page-13.jpg"/>
+                <img src="/images/Page-14.jpg"/>
+                <img src="/images/Page-15.jpg"/>
+                <img src="/images/Page-16.jpg"/>
+                <img src="/images/vin.jpg"/>
             </div>   
         </section>
     );
